cf0e9df79cfb5bb65ff91f1360be4aea6512ef8c
zjm <zjm@qq.com>
2019-12-27 17:56:39 +0800

    添加了一个增加功能

---
+++ src/Test/Type3.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Type3 {
+	public static void main(String[] args) {
+		add();
+		System.out.println("Äã´óÒ¯µÄe¡£¡£¡£¡£¡£");
+	}
+
+	private static void add() {
+		// TODO Auto-generated method stub
+		add1();
+	}
+	
+	public static void add1() {
+		System.out.println("");
+	}
+}
